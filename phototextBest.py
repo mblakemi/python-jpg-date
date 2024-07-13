6f7ad5ebaa16543594826bd7aee9b8c3a2f2d6d7
Michael Blake <mblaketx@gmail.com>
2024-07-13 18:59:26 -0500

    Updated DateToName for BW and shadows

--- phototextBest.py
+++ phototextBest.py
@@ -7,7 +7,15 @@
 #22-06-20 added ImageFile.LOAD_TRUNCATED_IMAGES = True
 #   to handle broken data stream when reading image file
 #22-07-20 Updated for Python 3.9.12
-
+#24-06-14 Add background (nTextShadow = 9), Mostly white text ((avg >245))
+#   and smaller gap from text to top (iystart = int(iystart*.05))
+#   currently use Python 3.9.18
+#   Added Try for draw.text line as error if BW jpg file
+#24-07-12 Added variables for bColoredText=False (black and white text/background)
+#    bColoredText is True if Colored text is used (default False)
+#    nTextShadow is number of copies of background text
+#    dMaxForWhite is the maximum sampled value below which white text
+#       is used.
 import os
 
 from PIL import Image
@@ -20,6 +28,12 @@ from PIL import ImageDraw
 #NOTE if you get this error, then rotate the image with windows viewer.
 bDebugReport = False
 
+
+# Text color and shadow
+bColoredText = False
+nTextShadow = 9 # should be odd
+dMaxForWhite = 180 # max avg test line value for white text (bColoredText = False)
+
 # Note either use 'out' as sOutDir and the parent directories to
 # be converted or use sFolder as the folder to be converted and
 # explicitly create this directory in out
@@ -31,7 +45,14 @@ sIndir = 'e:\\Archive\\Best Photo\\Best Photo 5\\' + sFolder
 sIndir = 'E:\\Temp\\Photos'
 #sIndir = 'e:\\Archive\\Best Photo\\Best Photo 1\\Selected HiRes'
 #sIndir = 'Rotate'
-sIndir = 'in'
+
+#determine if full source directory should be used
+bUseFullBestForIn = False
+if (bUseFullBestForIn):
+    sIndir = 'E:\\Archive\\Usb Drives\\2023 Davis Blake USB\\Best Photo'
+else:   
+    sIndir = 'in'
+
 
 
 sOutDir = 'out\\' + sFolder
@@ -41,6 +62,7 @@ ivalR = 255
 ivalG = 0
 ivalB = 255
 colLightRGB = (255,255,255) #was (200,200,255)
+colBlackRGB = (0,0,0)
 colDarkRGB = (0,0,200)
 colMiddleRGB = (0,255,0) #(255,80,0)
 colMinBlue = (0,0,255)
@@ -92,7 +114,11 @@ def add_photo_text(in_dir_name, out_dir_name, in_name):
     print('Not photo: [' + sInFileName+ ']')
     return
   # Use EXIF info to rotate image if necessary
-  iRotate = imageRotation(sInFileName)
+  try:
+      iRotate = imageRotation(sInFileName)
+  except:
+     print('Not Rotate photo: [' + sInFileName+ ']')
+     return     
   if iRotate == 6:
     transposed  = img.transpose(Image.ROTATE_270)
     print ('Rotated 270')
@@ -125,43 +151,90 @@ def add_photo_text(in_dir_name, out_dir_name, in_name):
   
   if height < width:
       iystart = int(fsize/2)
+  iystart = int(iystart*.05) # 2024 smaller gap to top
 
   font = ImageFont.truetype('Arialbd.ttf',fsize)
   #font = ImageFont.load_default().font
 
   # use black if very light
   (avg, ravg, gavg, bavg) = avg_pix(img, iystart + fsize/2)
-  if bDebugReport:
-    print("aRGB=",avg, ravg, gavg, bavg)
-  colRGB = colMiddleRGB 
-  if avg < 140:
-    colRGB = colLightRGB
-  elif avg > 159:
-    colRGB = colDarkRGB
+  if bColoredText:
+      if bDebugReport:
+        print("aRGB=",avg, ravg, gavg, bavg)
+      colRGB = colMiddleRGB 
+      if avg < 140:
+        colRGB = colLightRGB
+      elif avg > 159:
+        colRGB = colDarkRGB
+      else:
+        # assign minimum color
+        if ravg < gavg:
+          if ravg < bavg * 1.5:
+            colRGB = colMinRed
+          else:
+            colRGB = colMinBlue
+        else:
+          if gavg < bavg * 1.5:
+            colRGB = colMinGreen
+          else:
+            colRGB = colMinBlue
+      if bDebugReport:
+        print (colRGB)
   else:
-    # assign minimum color
-    if ravg < gavg:
-      if ravg < bavg * 1.5:
-        colRGB = colMinRed
-      else:
-        colRGB = colMinBlue
-    else:
-      if gavg < bavg * 1.5:
-        colRGB = colMinGreen
-      else:
-        colRGB = colMinBlue
-  if bDebugReport:
-    print (colRGB)
-         
+        # if (avg >160):
+        if (avg >dMaxForWhite):
+          colOutlineRGB = colLightRGB
+          colMainRGB = colBlackRGB 
+        else:
+           colOutlineRGB = colBlackRGB
+           colMainRGB = colLightRGB             
+       
   # draw.text((x, y),"Sample Text",(r,g,b))
-  stext=''
+  # stext=''
   # trim off .jpg or .xxx from text file
   if in_name[-4] == '.':
     photo_text = in_name[:-4]
     if (bOnlyAddDate):
       photo_text = in_name[0:10]
 #    print('iy', iystart, fsize)
-    draw.text((0, iystart),photo_text,colRGB,font=font)
+    if bColoredText:
+        draw.text((0, iystart),photo_text,colRGB,font=font)
+    else:
+       nTextMid = int(nTextShadow/2)
+       for ishx in range(0,nTextShadow) :
+           for ishy in range(0,nTextShadow) :
+                # print (colOutlineRGB)
+                try:
+                    draw.text((ishx, iystart+ishy),photo_text,colOutlineRGB,font=font)
+                except TypeError as error_text:
+                      print ('**********')
+                      print ('Could not Draw: ' + sInFileName)
+                      print(error_text)
+                      print ('**********')
+                      return
+     
+        # draw.text((0, iystart),photo_text,colOutlineRGB,font=font)
+        # draw.text((1, iystart),photo_text,colOutlineRGB,font=font)
+        # draw.text((3, iystart),photo_text,colOutlineRGB,font=font)
+        # draw.text((4, iystart),photo_text,colOutlineRGB,font=font)
+        # draw.text((0, iystart+1),photo_text,colOutlineRGB,font=font)
+        # draw.text((1, iystart+1),photo_text,colOutlineRGB,font=font)
+        # draw.text((3, iystart+1),photo_text,colOutlineRGB,font=font)
+        # draw.text((4, iystart+1),photo_text,colOutlineRGB,font=font)
+        # draw.text((0, iystart+2),photo_text,colOutlineRGB,font=font)
+        # draw.text((1, iystart+2),photo_text,colOutlineRGB,font=font)
+        # draw.text((3, iystart+2),photo_text,colOutlineRGB,font=font)
+        # draw.text((4, iystart+2),photo_text,colOutlineRGB,font=font)
+        # draw.text((0, iystart+4),photo_text,colOutlineRGB,font=font)
+        # draw.text((1, iystart+4),photo_text,colOutlineRGB,font=font)
+        # draw.text((3, iystart+4),photo_text,colOutlineRGB,font=font)
+        # draw.text((4, iystart+4),photo_text,colOutlineRGB,font=font)
+        # draw.text((0, iystart+5),photo_text,colOutlineRGB,font=font)
+        # draw.text((1, iystart+5),photo_text,colOutlineRGB,font=font)
+        # draw.text((3, iystart+5),photo_text,colOutlineRGB,font=font)
+        # draw.text((4, iystart+5),photo_text,colOutlineRGB,font=font) 
+        # draw.text((2, iystart+3),photo_text,colMainRGB,font=font)        
+       draw.text((nTextMid, iystart+nTextMid),photo_text,colMainRGB,font=font)   
   else :
     print('Did not recognize file type: ' + in_name)
   img.save(out_dir_name + in_name)
